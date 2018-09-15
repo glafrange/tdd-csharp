22bed4abc4282eed8ac2b7bd6d84a2820398308c
Gabriel Lafrange <gabriellafrange@gmail.com>
2018-09-15 12:25:58 -0400

    FibonacciNumbers

---
+++ FibonacciNumbers/FibonacciNumbers.Tests/FibonacciTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace FibonacciNumbers.Tests
+{
+	[TestFixture]
+    public class FibonacciTests
+    {
+		[Test]
+		public void WhenOneReturnsOne()
+		{
+			Assert.AreEqual(FibonacciNumbers.GetFib(1), 1);
+		}
+
+		[Test]
+		public void WhenThreeReturnsTwo()
+		{
+			Assert.AreEqual(FibonacciNumbers.GetFib(3), 2);
+		}
+
+		[Test]
+		public void WhenFiveReturnsFive()
+		{
+			Assert.AreEqual(FibonacciNumbers.GetFib(5), 5);
+		}
+    }
+}
---
+++ FibonacciNumbers/FibonacciNumbers/FibonacciNumbers.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace FibonacciNumbers
+{
+    public class FibonacciNumbers
+    {
+        static void Main(string[] args)
+        {
+			Console.WriteLine("enter num");
+			var num = Console.ReadLine();
+			var fib = GetFib(Convert.ToInt32(num));
+			Console.WriteLine("The {0} number in the fibonacci sequence is: {1}", num, fib);
+        }
+
+		public static int GetFib(int index)
+		{
+			var current = 1;
+			var last = 1;
+
+			for (int i = 2; i < index; i++)
+			{
+				current += last;
+				last = current - last;
+			}
+
+			return current;
+		}
+    }
+}
