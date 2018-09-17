d14f788d2cb558c5f0e298e06c498413971394bc
Gabriel Lafrange <gabriellafrange@gmail.com>
2018-09-17 12:41:10 -0400

    RomanNumeralParser

---
+++ RomanNumeralParser/RomanNumeralParser/RomanNumeralParser.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RomanNumeralParser
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine(RomanNumeralParser.Parse("MCDXXIII"));
+		}
+	}
+
+	public class RomanNumeralParser
+	{
+		private static Dictionary<char, int> numeralValues = 
+			new Dictionary<char, int>()
+			{
+				{'I', 1 },
+				{'V', 5 },
+				{'X', 10 },
+				{'L', 50 },
+				{'C', 100 },
+				{'D', 500 },
+				{'M', 1000 }
+			};
+
+		public static int Parse(string numeralsInput)
+		{
+			
+
+			char[] numerals = numeralsInput.ToCharArray();
+
+			int sum = 0;
+
+			for (int index = 0; index < numerals.Length; index++)
+			{
+				int numeralValue = numeralValues[numerals[index]];
+				int nextNumeralValue = 0;
+
+				if (index + 1 < numerals.Length)
+					nextNumeralValue = numeralValues[numerals[index + 1]];
+
+				if (numeralValue < nextNumeralValue)
+				{
+					sum += nextNumeralValue - numeralValue;
+					index++;
+				}
+				else
+				{
+					sum += numeralValue;
+				}
+
+			}
+
+			return sum;
+		}
+	}
+}
---
+++ RomanNumeralParser/RomanNumeralParserTests/RomanNumeralParserTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace RomanNumeralParser
+{
+	[TestFixture]
+	public class RomanNumeralParserTests
+	{
+		[Test]
+		public void Input_I_Returns_1()
+		{
+			Assert.AreEqual(RomanNumeralParser.Parse("I"), 1);
+		}
+
+		[TestCase("I", 1)]
+		[TestCase("V", 5)]
+		[TestCase("X", 10)]
+		[TestCase("L", 50)]
+		[TestCase("C", 100)]
+		[TestCase("D", 500)]
+		[TestCase("M", 1000)]
+		public void TestSingleNumeral(string input, int expectedOutput)
+		{
+			Assert.AreEqual(RomanNumeralParser.Parse(input), expectedOutput);
+		}
+
+		[TestCase("IV", 4)]
+		[TestCase("IX", 9)]
+		[TestCase("XL", 40)]
+		[TestCase("IL", 49)]
+		[TestCase("XD", 490)]
+		public void TestSubtractiveNotation(string input, int expectedOutput)
+		{
+			Assert.AreEqual(RomanNumeralParser.Parse(input), expectedOutput);
+		}
+		
+		[TestCase("MCDXXIII", 1423)]
+		[TestCase("DCCXCIII", 793)]
+		[TestCase("CCCXXV", 325)]
+		[TestCase("MMMDCXLIX", 3649)]
+		public void TestComplexNumerals(string input, int expectedOutput)
+		{
+			Assert.AreEqual(expectedOutput, RomanNumeralParser.Parse(input));
+		}
+	}
+}
