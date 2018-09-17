6858d3229c21816bf7e264633f616b157a16653d
Gabriel Lafrange <gabriellafrange@gmail.com>
2018-09-17 00:09:43 -0400

    FizzBuzz

--- FizzBuzz/FizzBuzzTests/FizzBuzzTests.cs
+++ FizzBuzz/FizzBuzzTests/FizzBuzzTests.cs
@@ -1,8 +1,34 @@
 ﻿using System;
+using NUnit.Framework;
 
 namespace FizzBuzzTests
 {
-    public class FizzBuzzTests
-    {
+	[TestFixture]
+	public class FizzBuzzTests
+	{
+		[Test]
+		public void DefaultOutputTest([Values(1,2,4,13,47)] int input)
+		{
+			Assert.AreEqual(Fizzbuzz.FizzBuzzer.CheckNumber(input), input.ToString());
+		}
+
+		[Test]
+		public void FizzOutputTest([Values(3,6,9,18,24)] int input)
+		{
+			Assert.AreEqual(Fizzbuzz.FizzBuzzer.CheckNumber(input), "Fizz");
+		}
+
+		[Test]
+		public void BuzzOutputTest([Values(5, 10, 25, 40, 65)] int input)
+		{
+			Assert.AreEqual(Fizzbuzz.FizzBuzzer.CheckNumber(input), "Buzz");
+		}
+
+		[Test]
+		public void FizzBuzzOutputTest([Values(15,30,45,60,75)] int input)
+		{
+			Assert.AreEqual(Fizzbuzz.FizzBuzzer.CheckNumber(input), "FizzBuzz");
+		}
+
     }
 }
--- FizzBuzz/Fizzbuzz/FizzBuzz.cs
+++ FizzBuzz/Fizzbuzz/FizzBuzz.cs
@@ -9,4 +9,28 @@ namespace Fizzbuzz
             Console.WriteLine("Hello World!");
         }
     }
+
+	public class FizzBuzzer
+	{
+		static public string CheckNumber(int number)
+		{
+			//return number % 3 == 0 ? "Fizz" : number.ToString();
+			if (number % 3 == 0 && number % 5 == 0)
+			{
+				return "FizzBuzz";
+			}
+			else if (number % 3 == 0)
+			{
+				return "Fizz";
+			}
+			else if (number % 5 == 0)
+			{
+				return "Buzz";
+			}
+			else
+			{
+				return number.ToString();
+			}
+		}
+	}
 }
